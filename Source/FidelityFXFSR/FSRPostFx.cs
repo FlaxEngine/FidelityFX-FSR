87f9e21ec6fb712a81735a2e720dda34dbeceb4a
Wojciech Figat <wojtek@figat.pl>
2022-12-13 11:52:08 +0100

    Update to support `RenderingUpscaleLocation.BeforePostProcessingPass`

--- Source/FidelityFXFSR/FSRPostFx.cs
+++ Source/FidelityFXFSR/FSRPostFx.cs
@@ -43,13 +43,11 @@ namespace FidelityFX
         {
             Profiler.BeginEventGPU("FSR");
 
-            var outputView = renderContext.Task.OutputView;
-            var outputViewport = renderContext.Task.OutputViewport;
             var inputSize = input.Size;
-            var outputSize = outputViewport.Size;
-            var outputWidth = (int)outputViewport.Width;
-            var outputHeight = (int)outputViewport.Height;
-            var desc = GPUTextureDescription.New2D(outputWidth, outputHeight, PixelFormat.R8G8B8A8_UNorm, GPUTextureFlags.ShaderResource | GPUTextureFlags.UnorderedAccess);
+            var outputSize = output.Size;
+            var outputWidth = output.Width;
+            var outputHeight = output.Height;
+            var desc = GPUTextureDescription.New2D(outputWidth, outputHeight, output.Format, GPUTextureFlags.ShaderResource | GPUTextureFlags.UnorderedAccess);
             var upscaled = RenderTargetPool.Get(ref desc);
             var sharpened = RenderTargetPool.Get(ref desc);
             var cb = _shader.GPU.GetCB(0);
@@ -65,23 +63,23 @@ namespace FidelityFX
                 context.UpdateCB(cb, new IntPtr(&data));
             }
 
+            // Upscale
             context.BindCB(0, cb);
             context.BindSR(0, input);
             context.BindUA(0, upscaled.View());
             context.Dispatch(_shader.GPU.GetCS("CS_Upscale"), (uint)(outputWidth + 15) / 16, (uint)(outputHeight + 15) / 16, 1);
             context.ResetUA();
 
+            // Sharpen
             context.BindSR(0, upscaled);
             context.BindUA(0, sharpened.View());
             context.Dispatch(_shader.GPU.GetCS("CS_Sharpen"), (uint)(outputWidth + 15) / 16, (uint)(outputHeight + 15) / 16, 1);
             context.ResetUA();
 
             // Copy pass
-            context.SetViewportAndScissors(outputViewport);
-            context.SetRenderTarget(outputView);
+            context.SetViewportAndScissors(outputWidth, outputHeight);
+            context.SetRenderTarget(output.View());
             context.Draw(sharpened);
-
-            // TODO: disable film grain and chromatic aberration from PostProcessing and apply if manually after FSR
 
             RenderTargetPool.Release(upscaled);
             RenderTargetPool.Release(sharpened);
